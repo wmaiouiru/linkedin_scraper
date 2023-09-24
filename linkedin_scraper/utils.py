73597fb38908dc07f98c5e2ad12194c3920cc148
wmaiouiru <wmaiouiru@gmail.com>
2023-09-23 21:02:40 -0700

    added integration test

--- linkedin_scraper/utils.py
+++ linkedin_scraper/utils.py
@@ -1,14 +1,24 @@
 """
 Utilities
 """
-
+import io
 from typing import Dict, Any, List
 from dataclasses import is_dataclass, asdict
 
-def to_dict(obj) -> Dict[str, Any]:
+
+def custom_asdict(obj):
+    # Custom asdict function that excludes _io.BufferedWriter objects
+    obj_dict = {}
+    for field in obj.__dataclass_fields__.values():
+        value = getattr(obj, field.name)
+        if not isinstance(value, io.BufferedWriter):
+            obj_dict[field.name] = to_dict(value)
+    return obj_dict
+
+def to_dict(obj) -> Dict[str, Any]:    
     if is_dataclass(obj):
         # If the object is a data class, use asdict to convert it to a dictionary
-        return to_dict(asdict(obj))
+        return to_dict(custom_asdict(obj))
     
     if isinstance(obj, (int, str, bool, float)):
         # If the object is a basic type, return it as is
@@ -21,7 +31,7 @@ def to_dict(obj) -> Dict[str, Any]:
     if isinstance(obj, dict):
         # If the object is a dictionary, recursively call to_dict on its values
         return {key: to_dict(value) for key, value in obj.items() if not key.startswith('_')}
-    
+
     if hasattr(obj, '__dict__'):
         # If the object has a __dict__ attribute, recursively call to_dict on its attributes
         return {key: to_dict(value) for key, value in obj.__dict__.items() if not key.startswith('_')}
